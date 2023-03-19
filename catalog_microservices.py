29433bfdd48ae3d3efb50417586faf2bea14a8fb
Tanishkagoenka <97675971+Tanishkagoenka@users.noreply.github.com>
2023-03-19 22:45:07 +0530

    Update catalog_microservices.py

--- catalog_microservices.py
+++ catalog_microservices.py
@@ -1,1 +1,2 @@
 // this file contains the program for the catalog microservice, one of the microservices that we are using out of other microservices. 
+//catalog basically deals the catalog of product that we have.
