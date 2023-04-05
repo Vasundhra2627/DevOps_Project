8c650faaf644c1e70308e810983e39b6ee8e11b7
Shivi Sharma <86220450+s-shr05@users.noreply.github.com>
2023-04-05 23:30:30 +0530

    Update payment_microservice.py

--- payment_microservice.py
+++ payment_microservice.py
@@ -1,2 +1,2 @@
-/this file is based on payment microservices which includes when user will put the products into the payment cart. 
+this file is based on payment microservices which includes when user will put the products into the payment cart. 
 //Payment Service is the central service for payment transaction management
